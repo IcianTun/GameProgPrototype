7e9d927483b4c4c9275e3289c178803c969c894d
Supasin Rungsittikul <31331704+IcianTun@users.noreply.github.com>
2020-03-21 19:02:28 +0700

    102

---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Phase
+{
+    Deploy
+}
+
+public class GameManagerScript : MonoBehaviour {
+    
+    private static GameManagerScript _instance;
+
+    public static GameManagerScript Instance { get { return _instance; } }
+
+
+
+	void NextTurn()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UnitType
+{
+    Tank,
+    Light,
+    Ranger
+}
+
+public class Unit : MonoBehaviour {
+
+    private int x, y;
+
+    public UnitType unitType;
+    
+    public int hp;
+    public int productionCost;
+
+    public int atk;
+    public int range;
+    public int moveRange;
+
+    public bool isUpgraded = false;
+
+    public void takeDamage(int damage)
+    {
+        if(unitType == UnitType.Tank)
+        {
+            damage -= 1;
+            damage = Mathf.Max(damage, 0);
+        }
+           
+        hp -= damage;
+    }
+}
