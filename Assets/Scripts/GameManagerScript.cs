5243690c807025666e23fb17a5b335f6134845a4
EARTHKK <Earth1226@gmail.com>
2020-03-24 19:05:49 +0700

    merge เต้

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -361,8 +361,8 @@ public class GameManagerScript : MonoBehaviour
                 currentPlayer.unitList.Add(unit);
                 currentPlayer.production -= unit.productionCost;
                 producedUnit.Add(unit);
-                var list = new List<Unit> { unit };
-                InfoText(list);
+                var unitlist = new List<Unit> { unit };
+                InfoText(unitlist);
             } else if (currentPlayer.production > 0) {
                 GameObject newUnitObj = Instantiate(selectedUnitPrefab);
                 newUnitObj.transform.position = clickedCell.transform.position + new Vector3(0, 0.1f, 0);
